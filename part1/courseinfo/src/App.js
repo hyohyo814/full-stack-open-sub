cdc9bd99ec14eeef31be0459509ebcc7a5232e58
hyohyo814 <133283025+hyohyo814@users.noreply.github.com>
2023-05-18 20:58:56 +0000

    Conversion of course contents into an object to include parts as an array with identifiers name and exercises

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,33 +1,39 @@
-const Content = ... {
-  return (
-    <div>
-      <Part ... />
-      <Part ... />
-      <Part ... />
-    </div>
-  )
-}
+
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-}
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
   }
-  const part3 = {
-    name: 'State of a compromise',
-    exercises: 14
+
+  const Content = ...{
+    return (
+      <div>
+        <Part .../>
+        <Part .../>
+        <Part .../>
+      </div>
+    )
   }
-  
+
   return (
     <div>
       <Header course={course} />
-      <Content ... />
-      <Total ... />
+      <Content parts={course} />
+      <Total parts={course} />
     </div>
   )
 }
