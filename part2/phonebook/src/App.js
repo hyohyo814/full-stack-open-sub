9ab0759eff36b84606d8145154718e90e829abbf
hyohyo814 <133283025+hyohyo814@users.noreply.github.com>
2023-05-26 06:08:47 +0000

    Added a filter function and forming the base code that controls the filtered display and allows for realtime rendering alongside text input

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,23 +6,27 @@ const App = () => {
   ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [showName, setShowName] = useState('')
+  const [filterName, setFilterName] = useState([
+    { name: '', number: '', id:'' }
+  ])
 
-  const addName = (event) => {
+  const addContact = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
 
     //prevent user from entering a blank input
-    if ( newName === '' ) {
+    if (newName === '') {
       alert(`Please input a valid name!`)
       return
     }
 
-    if (newNumber === '' ) {
+    if (newNumber === '') {
       alert(`Please input a valid number`)
       return
     }
 
-    const personObj ={
+    const personObj = {
       name: newName,
       number: newNumber,
       id: persons.length + 1,
@@ -40,10 +44,11 @@ const App = () => {
     //use the size function of Set to compare unique name entries and check for change in size
     //as indicator of a non unique entry
     const check = new Set()
-    console.log(check)
+    console.log(`precheck ${check}`)
     const nameExists = postPerson.some((persons) => check.size === check.add(persons.name).size)
     const numberExists = postPerson.some((persons) => check.size === check.add(persons.number).size)
-    
+    console.log(`postcheck ${check}`)
+
     console.log(nameExists)
 
     //alert the user if duplicate entry is inputted and prevent the code from updating the
@@ -70,25 +75,47 @@ const App = () => {
     setNewName(event.target.value)
   }
 
+  const handleNameFilter = (event) => {
+    console.log(`filtering name: ${event.target.value}`)
+    setShowName(event.target.value)
+    //console.log(showName)
+    const currShowName = event.target.value
+    const filterOut = persons.filter((persons) => persons.name.toLowerCase().indexOf(currShowName.toLowerCase()) >= 0)
+    console.log(filterOut)
+    setFilterName(filterOut)
+  }
+
+  console.log(filterName)
+
   const handleNumberChange = (event) => {
     console.log(`number: ${event.target.value}`)
     setNewNumber(event.target.value)
   }
 
+  const DisplayFilt = ({ props }) => (
+    props.map(persons => <p key={persons.id}>{persons.name} {persons.number}</p>)
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addContact}>
         <div>
-          name: <input 
-          value={newName} 
-          onChange={handleNameChange}
+          filter shown with: <input
+            value={showName}
+            onChange={handleNameFilter}
           />
         </div>
         <div>
-          number: <input 
-          value={newNumber}
-          onChange={handleNumberChange}
+          name: <input
+            value={newName}
+            onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
@@ -96,6 +123,9 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
+      <div>
+        <DisplayFilt key={filterName.id} props={filterName} />
+      </div>
     </div>
   )
 }
