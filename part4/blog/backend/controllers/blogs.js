9c84ee576325973485626d24fa5d617af32dbb4e
hyohyo814 <sojaisam@gmail.com>
2023-06-06 19:00:34 -0700

    Added controllers for viewing individual objects on the database and a delete function to remove blogs from the list

--- part4/blog/backend/controllers/blogs.js
+++ part4/blog/backend/controllers/blogs.js
@@ -1,10 +1,22 @@
 const Blog = require('../models/blog')
 const blogsRouter = require('express').Router()
 
+
 blogsRouter.get('/', (req, res) => {
   Blog.find({}).then((blogs) => {
     res.json(blogs)
   })
+})
+
+blogsRouter.get('/:id', (req, res, next) => {
+    Blog.findById(req.params.id).then(returnedBlog => {
+        if (returnedBlog) {
+            res.json(returnedBlog)
+        } else {
+            res.status(404).end()
+        }
+    })
+    .catch((err) => next(err))
 })
 
 blogsRouter.post('/', (req, res, next) => {
@@ -18,6 +30,13 @@ blogsRouter.post('/', (req, res, next) => {
     .catch((err) => {
       next(err)
     })
+})
+
+blogsRouter.delete('/:id', (req, res, next) => {
+    Blog.findByIdAndRemove(req.params.id).then(() => {
+        res.status(204).end()
+    })
+    .catch((err) => next(err))
 })
 
 module.exports = blogsRouter
