61b854974a79748b5d573203917a314a56973fa5
hyohyo814 <sojaisam@gmail.com>
2023-06-16 20:38:13 -0700

    Adjustments for E2E testing

--- part5/backend/.eslintrc.js
+++ part5/backend/.eslintrc.js
@@ -1,29 +1,36 @@
+/* eslint-env node */
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
-    es2021: true,
+    es6: true,
     'jest/globals': true,
+    'cypress/globals': true,
   },
-  extends: 'airbnb',
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
   parserOptions: {
-    ecmaVersion: 'latest',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
+  plugins: ['react', 'jest', 'cypress'],
   rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
-    'jest/no-disabled-tests': 'warn',
-    'jest/no-focused-tests': 'error',
-    'jest/no-identical-title': 'error',
-    'jest/prefer-to-have-length': 'warn',
-    'jest/valid-expect': 'error',
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-        optionalDependencies: true,
-        peerDependencies: true,
-      },
-    ],
+    'react/prop-types': 0,
+    'react/react-in-jsx-scope': 'off',
   },
-  plugins: ['jest'],
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
 };
