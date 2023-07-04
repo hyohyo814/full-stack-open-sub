0a97bfd686ba48bc1f3a7d01df6c988b6adcb4a8
hyohyo814 <sojaisam@gmail.com>
2023-07-03 20:07:45 -0700

    Updated new typeguards to accept new properties

--- part9/Patientor/patientor-backend/src/services/patientService.ts
+++ part9/Patientor/patientor-backend/src/services/patientService.ts
@@ -1,16 +1,17 @@
-import { v4 as uuidv4 } from 'uuid';
+/*import { v4 as uuidv4 } from 'uuid';*/
 import patients from '../../data/patients';
-import { Patient, PatientNonSensitive, NewPatient } from '../types';
+import { Patient, PatientNonSensitive } from '../types';
 
-const patientsData: Patient[] = patients;
+/*const patientsData: Patient[] = patients;*/
 
 const getData = (): PatientNonSensitive[] => {
-  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+  return patients.map(({ id, name, dateOfBirth, gender, occupation, entries }) => ({
     id,
     name,
     dateOfBirth,
     gender,
-    occupation
+    occupation,
+    entries
   }));
 };
 
@@ -22,19 +23,20 @@ const getOne = (id: string): Patient => {
   throw new Error('Could not find patient with id ' + id);
 };
 
+/*
 const postData = ( entry: NewPatient ): Patient => {
   const newPatient = {
     id: uuidv4(),
-    entries: [],
     ...entry
   };
 
   patientsData.push(newPatient);
   return newPatient;
 };
+*/
 
 export default {
   getData,
-  postData,
+  /*postData,*/
   getOne
 };
--- part9/Patientor/patientor-backend/src/types.ts
+++ part9/Patientor/patientor-backend/src/types.ts
@@ -6,14 +6,70 @@ export interface Diagnose {
 
 export type DiagnoseWOLatin = Omit<Diagnose, 'latin'>;
 
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export enum HealthCheckRating {
+  'Healthy' = 0,
+  'LowRisk' = 1,
+  'HighRisk' = 2,
+  'CriticalRisk' = 3,
+}
+
+export enum EntryType {
+  Hospital = 'Hospital',
+  OccupationalHealthcare = 'OccupationalHealthcare',
+  HealthCheck = 'HealthCheck'
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: EntryType;
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: EntryType;
+  discharge: Discharge;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+  type: EntryType;
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+type UnionOmit<T, K extends string | number | symbol> = T extends unknown
+  ? Omit<T, K>
+  : never;
+
+type ChangeFields<T, K> = Omit<T, keyof K> & K;
+
+export type EntryWithoutId = UnionOmit<Entry, 'id'>;
+
 export enum Gender {
   Male = 'male',
   Female = 'female',
   Other = 'other',
-}
-
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
 }
 
 export interface Patient {
@@ -26,6 +82,6 @@ export interface Patient {
   entries: Entry[];
 }
 
-export type NewPatient = Omit<Patient, 'id' | 'entries'>;
+export type NewPatient = Omit<ChangeFields<Patient, {entries: EntryWithoutId[]}>, 'id'>;
 
-export type PatientNonSensitive = Omit<Patient, 'ssn' | 'entries'>;
+export type PatientNonSensitive = Omit<Patient, 'ssn'>;
